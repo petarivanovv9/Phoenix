f6dbae5f93bed2932819ede72d964d8c3fa5437e
Petar Ivanov <pepincho95@gmail.com>
2016-09-01 21:48:35 +0300

    Add LoginForm class \to form file in website app.

--- phoenix/website/forms.py
+++ phoenix/website/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.TextInput(
+        label="Username",
+        widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': ''}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
