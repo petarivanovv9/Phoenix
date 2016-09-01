0ef199a1a8c81a0b00cdd8d7a3e0b3383142e8d6
Petar Ivanov <pepincho95@gmail.com>
2016-09-01 21:59:59 +0300

    Fix typo in LoginForm class.

--- phoenix/website/forms.py
+++ phoenix/website/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
-    username = forms.TextInput(
+    username = forms.CharField(
         label="Username",
         widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': ''}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
