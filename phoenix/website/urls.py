0f37e0582da8dcddcfddfae6645eb1d3b1088e21
Petar Ivanov <pepincho95@gmail.com>
2016-09-01 22:33:47 +0300

    Add login functionality for the user in website app.

--- phoenix/website/urls.py
+++ phoenix/website/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import (index, login, register)
+from .views import (index, log_in, register)
 
 
 urlpatterns = [
+    #'',
     url(r'^$', index, name='index'),
-    url(r'^login/$', login, name='login'),
+    url(r'^login/$', log_in, name='login'),
     url(r'^register/$', register, name='register'),
 ]
--- phoenix/website/views.py
+++ phoenix/website/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import (LoginForm)
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", locals())
 
 
-def login(request):
-    return render(request, "auth/login.html")
+def log_in(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("website:index"))
+            else:
+                error = "Invalid username or password"
+    return render(request, "auth/login.html", locals())
 
 
 def register(request):
-    return render(request, "auth/register.html")
+    return render(request, "auth/register.html", locals())
