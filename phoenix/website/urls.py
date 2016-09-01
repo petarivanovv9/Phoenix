0c1ade70c44eb4a31a2796402de2a4ad7d5ee3d7
Petar Ivanov <pepincho95@gmail.com>
2016-09-01 19:26:53 +0300

    Add login and register pages.

--- phoenix/website/urls.py
+++ phoenix/website/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import (index)
+from .views import (index, login, register)
 
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^login/$', login, name='login'),
+    url(r'^register/$', register, name='register'),
 ]
--- phoenix/website/views.py
+++ phoenix/website/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def login(request):
+    return render(request, "auth/login.html")
+
+
+def register(request):
+    return render(request, "auth/register.html")
