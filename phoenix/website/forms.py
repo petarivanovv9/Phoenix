841c276fc2f807cc18f3e8d964e3c0a1cf5f7635
Petar Ivanov <pepincho95@gmail.com>
2016-09-01 22:57:33 +0300

    Add login page initial design.

--- phoenix/website/forms.py
+++ phoenix/website/forms.py
@@ -6,5 +6,6 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(
         label="Username",
-        widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': ''}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+        widget=forms.TextInput(attrs={'class': 'pure-input-1', 'placeholder': 'Username', 'autofocus': ''}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'pure-input-1', 'placeholder': 'Password'}))
