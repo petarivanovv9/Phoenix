291ab7632ac96adf8ef098b279e993c14e645859
Petar Ivanov <pepincho95@gmail.com>
2016-09-03 20:45:06 +0300

    Add website tests definitions.

--- phoenix/website/tests/test_views.py
+++ phoenix/website/tests/test_views.py
@@ -1,10 +1,41 @@
+from django.test import Client
+from django.core.urlresolvers import reverse
+
 from django.test import TestCase
+
+from django.contrib.auth.models import User
 
 
 class TestWebsite(TestCase):
 
     def setUp(self):
-        pass
+        self.client = Client()
 
     def test_index(self):
+        url = reverse('website:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_from_active_user(self):
+        pass
+
+    def test_login_from_unactive_user(self):
+        pass
+
+    def test_login_with_invalid_data(self):
+        pass
+
+    def test_login_wrong_pass(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+    def test_logout_login_required(self):
+        pass
+
+    def test_register_get_query(self):
+        pass
+
+    def test_register_if_not_registered(self):
         pass
