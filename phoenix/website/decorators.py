21045ef7e5619d915b19c29bd10e5cfc399ddf82
Petar Ivanov <pepincho95@gmail.com>
2016-09-03 16:47:52 +0300

    Implement anonymous_required func, redirect user if he've already logged in.

--- phoenix/website/decorators.py
+++ phoenix/website/decorators.py
@@ -2,4 +2,9 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def anonymous_required(redirect_url):
-    pass
+    decorator = user_passes_test(
+            test_func=lambda u: not u.is_authenticated(),
+            login_url=redirect_url,
+            redirect_field_name=None
+    )
+    return decorator
--- phoenix/website/views.py
+++ phoenix/website/views.py
@@ -1,14 +1,16 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import (LoginForm)
+from .decorators import anonymous_required
 
 
 def index(request):
     return render(request, "index.html", locals())
 
 
+@anonymous_required(redirect_url=reverse_lazy('website:index'))
 def log_in(request):
     form = LoginForm()
     if request.method == 'POST':
@@ -19,7 +21,7 @@ def log_in(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect(reverse("website:index"))
+                return redirect(reverse('website:index'))
             else:
                 error = "Invalid username or password"
     return render(request, "auth/login.html", locals())
