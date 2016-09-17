16bf1f0835080349feb5f75c93af80321a85bf2d
Petar Ivanov <pepincho95@gmail.com>
2016-09-17 17:02:08 +0300

    Add UserProfile, Chat and Message models to messenger app.

--- phoenix/messenger/models.py
+++ phoenix/messenger/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+
+
+class Chat(models.Model):
+    name = models.CharField(max_length=20, blank=True, default="", null=True)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    content = models.CharField(max_length=255, blank=True, default="", null=True)
+    chat = models.ForeignKey(Chat)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{} - {}".format(chat.name, user.username)
