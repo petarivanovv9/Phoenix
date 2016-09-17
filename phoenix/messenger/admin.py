4cfc809ccb54c774fc89074306deb68d3123c9fe
Petar Ivanov <pepincho95@gmail.com>
2016-09-17 19:35:41 +0300

    Register Chat and Message models in admin panel

--- phoenix/messenger/admin.py
+++ phoenix/messenger/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (Chat, Message)
 
-# Register your models here.
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
