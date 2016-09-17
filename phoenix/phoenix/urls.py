2f66a9b80bc6e423984e2c9fe0a0159e5dd49cc9
Petar Ivanov <pepincho95@gmail.com>
2016-09-17 17:01:09 +0300

    Add initial urls file to messenger app.

--- phoenix/phoenix/urls.py
+++ phoenix/phoenix/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('website.urls', namespace='website')),
+    url(r'^', include('messenger.urls', namespace='messenger')),
 ]
